42cf812618843b338325f69e965da83fb3e8af7a
Morgan Smith <londonspleen@gmail.com>
2026-04-13 10:29:38 -0400

    very important

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -13,8 +13,8 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+          source={{uri:"https://hips.hearstapps.com/hmg-prod/images/white-cat-breeds-kitten-in-grass-67bf648a54a3b.jpg"}}
+          style={{ width: '100%', height: '100%' }}
         />
       }>
       <ThemedView style={styles.titleContainer}>
